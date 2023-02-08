263225ce7f3a17cff8af7053a41ef1d8c9aa06bb
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-02-08 10:13:38 +0100

    add getAllAppsFromDb

--- src/main/java/com/kristofferph/security/mapper/UserMapper.java
+++ src/main/java/com/kristofferph/security/mapper/UserMapper.java
@@ -1,16 +1,9 @@
 package com.kristofferph.security.mapper;
 
-import com.kristofferph.security.game.App;
-import com.kristofferph.security.game.AppResponse;
 import com.kristofferph.security.user.User;
 import com.kristofferph.security.user.UserResponse;
-import com.kristofferph.security.user.UserService;
 import com.kristofferph.security.user.UserUpdatedResponse;
-import org.hibernate.cfg.annotations.reflection.internal.XMLContext;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
